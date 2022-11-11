74978a95c375bb6496873b9358ddc49d24235f14
ftyler <ftyler.ctr@kr.af.mil>
2022-11-10 19:40:33 -0500

    Migrate feature -a to yarn.

---
+++ feature-a/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Feature A Version 1.0.0 %VERSION%
+      </header>
+    </div>
+  );
+}
+
+export default App;
