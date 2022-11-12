e0458fa0aa14c427117923c56cd3365e546eb6a1
ftyler <ftyler.ctr@kr.af.mil>
2022-11-12 10:59:25 -0500

    A change to feature b.

--- feature-b/src/App.js
+++ feature-b/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Feature B Final Build
+        Feature B Final
       </header>
     </div>
   );
