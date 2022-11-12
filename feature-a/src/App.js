4c12522efc01cebef1d11a7451231a03405da17d
ftyler <ftyler.ctr@kr.af.mil>
2022-11-12 09:51:38 -0500

    Test build.

--- feature-a/src/App.js
+++ feature-a/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Feature A Use Docker Metadata Action
+        Feature A Use Docker Metadata
       </header>
     </div>
   );
