6720759d6ff680138df83ff93a24352acde34ec3
ftyler <ftyler.ctr@kr.af.mil>
2022-11-12 07:05:37 -0500

    Use action.

--- feature-a/src/App.js
+++ feature-a/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Feature A (Version 1.1.1)
+        Feature A Use Action instead (Version 1.1.1)
       </header>
     </div>
   );
